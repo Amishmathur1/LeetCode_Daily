151b94dee03d7c513b44127b7d3672d75734f543
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-06-03 13:48:45 +0530

    Time: 3 ms (82.50%) | Memory: 20.5 MB (61.37%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,10 +1,8 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        l, r = 0, 0
-        for i in range(len(nums)):
+        l = 0 
+        for r in range(len(nums)):
             if nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
+                nums[l], nums[r] = nums[r], nums[l] #swap
                 l += 1
-                r += 1
-            else:
-                r += 1
+            
