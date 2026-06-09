99755665de92d133b19c0ed0372367f562882ec9
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-06-09 22:51:02 +0530

    Time: 51 ms (6.10%) | Memory: 36.4 MB (6.61%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # if len(set(nums)) == len(nums):
@@ -5,14 +6,20 @@ class Solution:
         # else:
         #   
         
-        d = {}
-        for i in nums:
-            if i not in d:
-                d[i] = 1
-            else:
-                d[i] += 1
 
-        # print(d.values())
+        # d = {}
+        # for i in nums:
+        #     if i not in d:
+        #         d[i] = 1
+        #     else:
+        #         d[i] += 1
+        d = defaultdict(int)
+
+        for i in nums:
+            d[i] += 1
+
+        # print(d)
+
         for i in d.values():
             if i > 1:
                 return True
