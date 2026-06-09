6e315fcc9fba846837ac7f20962b536e66a22cb2
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-06-09 22:46:32 +0530

    Time: 39 ms (11.31%) | Memory: 36.3 MB (6.61%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,8 +4,16 @@ class Solution:
         #     return False
         # else:
         #   
-        freq = Counter(nums)
-        for i in freq.values():
+        
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+
+        # print(d.values())
+        for i in d.values():
             if i > 1:
                 return True
         return False
