592a47c8e1ed039c9f369a993e9411f3bb990085
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-06-19 12:51:56 +0530

    Time: 303 ms (99.10%) | Memory: 20.2 MB (30.18%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+            
+        dq = deque([amount])
+        coins_needed = 0
+        visited = {amount}
+
+        while dq:
+            size = len(dq)
+            coins_needed += 1
+
+            for _ in range(size):
+                value = dq.popleft()
+                for j in coins:
+                    new_val = value - j
+
+                    if new_val == 0:
+                        return coins_needed
+
+                    if new_val > 0 and new_val not in visited:
+                        dq.append(new_val)
+                        visited.add(new_val)
+        return -1
