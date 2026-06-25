7f09ec7599b7b8071e6e3ee4a4eb49d7a50bd886
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-06-25 10:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (5.14%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        ans = []
+        nums.sort()
+        visited = set()
+
+        def dfs(ind, arr):
+            if ind >= len(nums) and tuple(arr) not in visited:
+                ans.append(arr[:])
+                visited.add(tuple(arr))
+                return 
+            elif tuple(arr) in visited:
+                return 
+            
+            #pick 
+            arr.append(nums[ind])
+            dfs(ind+1, arr)
+
+            #not pick
+            arr.pop()
+            dfs(ind+1, arr)
+        dfs(0, [])
+        return ans
