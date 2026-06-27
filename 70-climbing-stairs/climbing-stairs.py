07410dfb317b32cf450ebce4470cc9fe8ff7e63d
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-06-27 14:36:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (16.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,15 +1,17 @@
+from functools import cache
 class Solution:
     
     def climbStairs(self, n: int) -> int:
         
-        memo ={}
+        # memo ={}
+        @cache
         def cal(s:int)->int:
-            if s in memo:
-                return memo[s]
+            # if s in memo:
+            #     return memo[s]
             if s <=2:
                 return s
         
-            memo[s] = cal(s-1)+cal(s-2)
-            return memo[s]
+            return cal(s-1)+cal(s-2)
+            # return memo[s]
         return cal(n)
 
