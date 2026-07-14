cf70d305a0ae1081e13084a501cb1158a5bd547c
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-07-14 19:49:34 +0530

    Time: 3 ms (75.15%) | Memory: 19.3 MB (78.73%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,47 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        dq = deque([])
+        visited = set()
+        cnt = 0
+
+        row, col = len(grid), len(grid[0])
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 0:
+                    cnt += 1
+                if grid[r][c] == 2:
+                    dq.append((r,c))
+                    visited.add((r,c))
+        
+        if cnt == row*col:
+            return 0
+
+        count = 0
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while dq:
+            size = len(dq)
+            count += 1
+
+            for _ in range(size):
+                r, c = dq.popleft()
+                for dr, dc in dirs:
+                    nr, nc = r + dr, c + dc
+                    
+                    if (nr < 0 or nr >= row or
+                        nc < 0 or nc >= col or
+                        grid[nr][nc] != 1 or 
+                        (nr, nc) in visited):
+                        continue
+
+                    grid[nr][nc] = 2
+                    visited.add((nr, nc))
+                    dq.append((nr, nc))
+        
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 1:
+                    return -1
+        
+        return count - 1
