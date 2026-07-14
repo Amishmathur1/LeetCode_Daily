9aeaac4504c5ac1b57079a172ac577a6f5ad8df3
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-07-14 19:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.63%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,17 +1,18 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
+        visited = set()
+        new_num = str(n)
 
-        while n != 1:
-            if n in seen:
+        while 1:
+            x = 0
+            for i in new_num:
+                x += int(i) ** 2
+
+            if x == 1:
+                return True
+
+            if x in visited:
                 return False
 
-            seen.add(n)
-
-            total = 0
-            for digit in str(n):
-                total += int(digit) ** 2
-
-            n = total
-
-        return True
+            visited.add(x)
+            new_num = str(x)
