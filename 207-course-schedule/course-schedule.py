9e32d08bac999abd3ed8efc58cf57d98d2a7f6db
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-07-23 17:07:48 +0530

    Time: 7 ms (44.35%) | Memory: 20.3 MB (78.28%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        ind = [0]*numCourses
+        ans = []
+        for u,v in prerequisites:
+            adj[u].append(v)
+            ind[v] +=1
+        q = deque([i for i in range(numCourses) if ind[i]==0])
+        while q:
+            curr = q.popleft()
+            ans.append(curr)
+            for n in adj[curr]:
+                ind[n] -=1
+                if ind[n]==0:
+                    q.append(n)
+        if len(ans) == numCourses:
+            return True
+        return False
