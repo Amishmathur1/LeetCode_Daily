05c24dfc562dcc896cb597c371367e96fd382b0c
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-07-24 19:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -3,20 +3,11 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        a1 = nums[:n-1]
-        a2 = nums[1:]
-
-        def func(ind,arr):
-            if ind >= len(arr):
-                return 0
-            if dp[ind] != -1:
-                return dp[ind]
-            dp[ind] = max(arr[ind] + func((ind + 2), arr), func((ind + 1), arr))
-            return dp[ind]
-
-        dp = [-1] * len(a1)
-        ans1 = func(0, a1)
-        dp = [-1] * len(a1)
-        ans2 = func(0, a2)
-
+        def tab(arr):
+            dp = [0] * (len(arr) + 2)
+            for i in range(len(arr)-1, -1, -1):
+                dp[i] = max(arr[i] + dp[i+2], dp[i+1])
+            return dp[0]
+        ans1 = tab(nums[:n-1])
+        ans2 = tab(nums[1:])    
         return (max(ans1, ans2))
