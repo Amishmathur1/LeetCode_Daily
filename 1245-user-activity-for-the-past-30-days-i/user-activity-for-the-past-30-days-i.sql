b7d4615c0f155a836f017e08109a3aca1b30eefa
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-09-09 21:55:23 +0530

    Time: 956 ms (54.44%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -3,5 +3,5 @@
 SELECT activity_date as day, count(DISTINCT user_id) as active_users
 FROM Activity
 GROUP BY activity_date
-HAVING activity_date > '2019-06-27'
+HAVING activity_date >= '2019-06-28'
    AND activity_date <= '2019-07-27';
